04fd18b6cef70511dc075a545c528f7122a182d5
Marielle <manlulum@myumanitoba.ca>
2019-11-09 19:59:12 -0600

    Shoot bullet but only upwards
    hey

--- Fragile World/Assets/JamPack/Code/PlayerController/PlayerController.cs
+++ Fragile World/Assets/JamPack/Code/PlayerController/PlayerController.cs
@@ -5,64 +5,89 @@ using UnityEngine;
 // This controls the player
 public class PlayerController : MonoBehaviour {
 
-    [Header("*** Input Variables ***")]
-    public bool buttonDown = false;
-    // set up a vector 2 to hold the input from the player
-    public Vector2 inputVector = Vector2.zero;
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Camera cam;
 
-    [Header("*** Player State ***")]
-    public float movementForce = 500f;
-    public float boostForce = 1000f;
+    Vector2 movement;
+    Vector2 mousePos;
 
-    // Other Classes on the GameObject
-    // The Rigidbody is needed to move items within the physics system
-    public Rigidbody2D playerRigidbody;
 
-    [Header(" *** Player State Information ***")]
+    void Update(){
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
-    // is the player activated or not? 
-    public bool autoStart = true;
-    public bool isActivated = true;
-
-    // Use this for initialization - 
-    // Start is called automatically when the object starts for the first time
-    void Start () {
-        // grab the physics object so we can move it
-         playerRigidbody = GetComponent<Rigidbody2D>();
-        if( !isActivated ){
-            isActivated = autoStart; // enable the player
-        }
-         
     }
 
-    // FixedUpdate is called once per Physics step (this happens more then once per frame, usually ~150-200 steps per second)
-    // We're using Physics so put it in Fixed Update rather then Update
-    void FixedUpdate () {
-        if(isActivated){
-            // Gets the Jump Button
-            buttonDown = Input.GetButton("Jump");
-            // you can also use .GetButtonDown("Jump") to only only register true on the single frame it was hit, useful for UI's
+    void FixedUpdate(){
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
-            // Gets the input values from the arrow keys (by default)
-            inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-            // this will return us a value for x and y between -1 and 1
-            // you can also do them seperately and handle the x axis and y axis each on thier own float
+        
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+;    }
+
+    // [Header("*** Input Variables ***")]
+    // public bool buttonDown = false;
+    // // set up a vector 2 to hold the input from the player
+    // public Vector2 inputVector = Vector2.zero;
+
+    // [Header("*** Player State ***")]
+    // public float movementForce = 500f;
+    // public float boostForce = 1000f;
+
+    // // Other Classes on the GameObject
+    // // The Rigidbody is needed to move items within the physics system
+    // public Rigidbody2D playerRigidbody;
+
+    // [Header(" *** Player State Information ***")]
+
+    // // is the player activated or not? 
+    // public bool autoStart = true;
+    // public bool isActivated = true;
+
+    // // Use this for initialization - 
+    // // Start is called automatically when the object starts for the first time
+    // void Start () {
+    //     // grab the physics object so we can move it
+    //      playerRigidbody = GetComponent<Rigidbody2D>();
+    //     if( !isActivated ){
+    //         isActivated = autoStart; // enable the player
+    //     }
+         
+    // }
+
+    // // FixedUpdate is called once per Physics step (this happens more then once per frame, usually ~150-200 steps per second)
+    // // We're using Physics so put it in Fixed Update rather then Update
+    // void FixedUpdate () {
+    //     if(isActivated){
+    //         // Gets the Jump Button
+    //         buttonDown = Input.GetButton("Jump");
+    //         // you can also use .GetButtonDown("Jump") to only only register true on the single frame it was hit, useful for UI's
+
+    //         // Gets the input values from the arrow keys (by default)
+    //         inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    //         // this will return us a value for x and y between -1 and 1
+    //         // you can also do them seperately and handle the x axis and y axis each on thier own float
 
 
-            // Physics - player
+    //         // Physics - player
             
 
-            if(buttonDown){
-                playerRigidbody.AddForce(inputVector * boostForce);
-            }else{
-                playerRigidbody.AddForce(inputVector * movementForce);
-            }
-        }
-    }
+    //         if(buttonDown){
+    //             playerRigidbody.AddForce(inputVector * boostForce);
+    //         }else{
+    //             playerRigidbody.AddForce(inputVector * movementForce);
+    //         }
+    //     }
+    // }
 
-    // Basic Accessor methods
-    // Allows other scripts to get information about this script without making variables public. 
-    public bool isActive() {
-        return isActivated;
-    }
+    // // Basic Accessor methods
+    // // Allows other scripts to get information about this script without making variables public. 
+    // public bool isActive() {
+    //     return isActivated;
+    // }
 }
